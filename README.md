a562b79bfa8402a64b1d9e5740607f9562f2077f
kyleawayan <56655579+kyleawayan@users.noreply.github.com>
2021-01-04 23:32:55 -0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # spotify-canvases
-Video URLs from Spotify Canvas
+
+Video URLs from [Spotify Canvas](https://canvas.spotify.com/en-us)
+
+Note that these are obtained manually, as the links of the Canvases are random hashes. They do not relate to the track ID or anything in the [Get a Track endpoint](https://developer.spotify.com/documentation/web-api/reference/tracks/get-track/).
+
+## How to get the Canvases
+
+To get the Canvas of a song, you need to find out the request the Spotify mobile app is making by intercepting the network traffic from your mobile device.
+
+Spotify Canvases are from `https://canvaz.scdn.co`.
+
+On iOS, we use an app called [Charles Proxy](https://apps.apple.com/us/app/charles-proxy/id1134218562). They also have a [trial version available for desktop](https://www.charlesproxy.com/), where you can make your computer act as a proxy for your device. There are other programs that do this too, but just make sure that whatever program you're using has the ability to intercept HTTPs/SSL traffic.
+
+## Contributing
+
+If you would like to add some Spotify Canvas links, please submit a pull request. You could either edit the `canvases.json` file on GitHub, or clone this repository. We have made a Python script that lets you easily add to the file. **We will only accept pull requests with video URLs from `canvaz.scdn.co`**. We will not accept third-party links.
---
+++ addCanvas.py
@@ -0,0 +1,97 @@
+import json
+import re
+import requests
+import time
+import sys
+
+data = None
+
+try:
+    f = open('canvases.json')
+    data = json.load(f)
+except:
+    print('')
+    print('\033[91m\033[1mError\033[0m')
+    print('File \033[1mcanvases.json\033[0m does not exist')
+    print('')
+    sys.exit(0)
+
+
+def askInfo(repeated):
+    if repeated == False:
+        print('')
+        print(
+            "Hi, thank you for contributing to \033[92m\033[1mspotify-canvases\033[0m!")
+    else:
+        print('')
+
+    uri = input(
+        "\033[1mPlease enter the track URI\033[0m › ")
+
+    for urisInFile in data["canvases"]:
+        if urisInFile["uri"] == uri:
+            print('')
+            print('\033[91m\033[1mError\033[0m')
+            print('Track already exists in file')
+            print('')
+            repeat(True)
+
+    songInfoReq = requests.get(
+        'https://azx57uenaj.execute-api.us-west-1.amazonaws.com/production/getUriInfo', {'uri': uri})
+
+    songInfo = songInfoReq.json()
+
+    if songInfoReq.status_code == 200:
+        songName = songInfo["message"]["name"]
+        songArtist = songInfo["message"]["artists"][0]["name"]
+        print('')
+        print(f"""\033[1m{songName}\033[0m
+{songArtist}""")
+        print('')
+        time.sleep(0.3)
+        videoUrl = input(
+            "\033[1mPlease enter the video URL\033[0m › ")
+        addToJson(uri, songName, songArtist, videoUrl, repeated)
+    else:
+        print('')
+        print('\033[91m\033[1mError\033[0m')
+        print(songInfo["message"])
+        print('')
+        repeat(True)
+
+
+def addToJson(uri, songName, songArtist, videoUrl, repeated):
+    data['canvases'].append({
+        "title": songName,
+        "artist": songArtist,
+        "uri": uri,
+        "canvas": videoUrl
+    })
+    with open("canvases.json", "w") as data_file:
+        json.dump(data, data_file, indent=4)
+    print('')
+    print("\033[92m\033[1mSuccessfully added!\033[0m")
+    if repeated == False:
+        print("Please make a pull request on the GitHub repository!")
+    print('')
+    repeat(False)
+
+
+def repeat(error):
+    if error:
+        time.sleep(0.2)
+        repeat = input("Would you like to try again? › [y/n] ")
+        if repeat.lower() == "y" or repeat.lower == "yes":
+            askInfo(True)
+        else:
+            sys.exit(0)
+    else:
+        time.sleep(0.1)
+        repeat = input("Would you like to add another track? › [y/n] ")
+        if repeat.lower() == "y" or repeat.lower == "yes":
+            askInfo(True)
+        else:
+            sys.exit(0)
+
+
+askInfo(False)
